85e95f045ea82d3d87187cb51dbe367d1ef1e8b0
Arthur Alves <arthur.rodriguesgv@gmail.com>
2025-03-27 14:41:47 -0300

    adicionar disciplina e curso

--- Lista_Aninhada_2_Tarefa.cpp
+++ Lista_Aninhada_2_Tarefa.cpp
@@ -8,7 +8,7 @@ typedef struct tipoDisciplina {
 	string nome;
 	int cargaHoraria;
 	tipoDisciplina *prox;
-	TPreRequisito *preRequisitos;	
+	struct tipoPreRequisito *preRequisito;
 }TDisciplina;
 
 typedef struct tipoPreRequisito {
@@ -34,18 +34,15 @@ typedef struct tipoLista {
 	TDisciplina *disciplinas;
 }TLista;
 
-
 void inicializa(TLista *lista){
 	lista->cursos = NULL;
 	lista->disciplinas = NULL;	
-
 }
 
 int menu(){
 	
 	int opcao;
 	system("CLS");
-	
 	printf("\n\n\n\t\t=======| MENU |=======\n\n");
 	
 	printf("\t0 - SAIR (Encerrar Aplicacao).\n\n");
@@ -57,11 +54,15 @@ int menu(){
 	printf("\t6 - Excluir PRE-REQUISITO de DISCIPLINA.\n\n");
 	printf("\t7 - Registrar GRADE CURRICULAR para CURSO.\n");
 	printf("\t8 - Excluir item da GRADE CURRICULAR de CURSO.\n\n");
+	printf("\t9 - Listar DISCIPLINAS.\n");
+	printf("\t10- Listar CURSOS.\n");
+	printf("\t11- Listar PRE_REQUISITOS de DISCIPLINA especifica.\n");
+	printf("\t12- Listar GRADE CURRICULAR de CURSO especifico.\n\n");
 	
 	printf("\t\tInforme a OPCAO desejada: ");
 	scanf("%d", &opcao);
 	
-	if((opcao < 0) || (opcao > 8)){
+	if((opcao < 0) || (opcao > 12)){
 		printf("\n\n\n\tERRO: OPCAO invalida!\n\tTente Novamente\n\n");
 		system("PAUSE");
 	}
@@ -74,30 +75,56 @@ void insertDisciplina(TLista *lista) {
 	nova = (TDisciplina *)malloc(sizeof(TDisciplina));
 	nova->prox = NULL;
 
-	printf("\nCadastro de DISCIPLINA: ");
+	printf("\nCadastro de DISCIPLINA");
 	printf("\nNOME do DISCIPLINA: ");
 	fflush(stdin);
 	gets(nova->nome);
 
-	printf("\nCARGA HORARIA da DISCIPLINA: ");
+	printf("CARGA HORARIA da DISCIPLINA: ");
 	fflush(stdin);
 	scanf("%d", &nova->cargaHoraria);
-	
+
 	if (lista->disciplinas == NULL) {
-		lista->disciplinas == nova;
+		lista->disciplinas = nova;
 	} else {
 		atual = lista->disciplinas;
+		
 		while (atual->prox != NULL) {
 			atual = atual->prox;
 		}
 		atual->prox = nova;
+
 	}
 
 }
 
 void deleteDiscipline(TLista *lista) {
 
+
+}
+
+void insertCurso(TLista *lista) {
+	TCurso *nova, *atual;
 	
+	nova = (TCurso *)malloc(sizeof(TCurso));
+	nova->prox = NULL;
+
+	printf("\nCadastro de CURSO");
+	printf("\nNOME do Curso: ");
+	fflush(stdin);
+	gets(nova->nome);
+
+	if (nova->nome == NULL) {
+		lista->cursos = nova;
+	} else {
+		atual = lista->cursos;
+		
+		while (atual->prox != NULL) {
+			atual = atual->prox;
+		}
+		atual->prox = nova;
+
+	}
 }
 
 int main(){
@@ -110,13 +137,23 @@ int main(){
 		opcao = menu();
 		
 		switch(opcao){
-			case 0: break;
-			case 1: insertDiscipline(lista); break;
-			case 2: deleteDiscipline(lista); break;
-			case 3: break;
-			case 4: break;
-			case 5: break;
-			case 6: break;
+			case 0: 
+				break;
+			case 1: 
+				insertDisciplina(lista);
+				break;
+			case 2: 
+				deleteDiscipline(lista); 
+				break;
+			case 3: 
+				insertCurso(lista)
+				break;
+			case 4: 
+				break;
+			case 5: 
+				break;
+			case 6: 
+				break;
 		}
 		
 	}while(opcao != 0);
